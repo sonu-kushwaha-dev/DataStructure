f1d0daba5b2d3fa7238c66e59e5a99681b9048f8
Sonu Kushwaha <kushwahasonu1319@gmail.com>
2025-03-16 14:18:01 +0530

    CircularDoublyLinkedList
    CircularDoublyLinkedList

--- DS2_LinkedList/src/com/app/circular/doublylinkedlist/CircularDoublyLinkedList.java
+++ DS2_LinkedList/src/com/app/circular/doublylinkedlist/CircularDoublyLinkedList.java
@@ -7,7 +7,6 @@ public class CircularDoublyLinkedList<T> {
 	public CircularDoublyLinkedList() {
 		this.head = null;
 	}
-
 	public void append(T data) {
 
 		Node<T> newNode = new Node<T>(data);
@@ -49,15 +48,64 @@ public class CircularDoublyLinkedList<T> {
 			} while (curr_node != head);
 		}
 	}
+	
+	private void delete(T data) {
+		
+		if(head==null) {
+			System.out.println("No Data");
+		}
+		else if (head.data.equals(data) && head.next==head && head.pre==head) {
+			head=null;
+		}
+		
+		else if (head.data.equals(data)) {
+			
+			head.next.pre=head.pre;
+			head.pre.next=head.next;
+			
+			head=head.next;
+		}
+		else {
+			Node <T> curr_node= head;
+			do {
+				curr_node=curr_node.next;
+			} while (curr_node!=head && !curr_node.data.equals(data));
+			
+			if(curr_node.data.equals(data)) {
+				
+				curr_node.next.pre=curr_node.pre;
+				curr_node.pre.next=curr_node.next;
+			
+			}
+			else {
+				System.out.println("No data");
+			}
+		}
+		
+		
+	}
 
 	public static void main(String[] args) {
 
 		CircularDoublyLinkedList<Integer> node = new CircularDoublyLinkedList<>();
+		
 		node.append(12);
 		node.append(13);
 		node.append(14);
 		node.append(15);
 		node.traversal();
+		
+		node.delete(12);
+		System.out.println("\n------------------------------\n");
+		node.traversal();
+		
+		System.out.println("\n");
+		node.delete(15);
+		
+		
+		System.out.println("\n------------------------------");
+		node.traversal();
+		
+		
 	}
-
 }
