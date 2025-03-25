1ca7eb24a9fc0acb1623dcd551ac87ec1d69e0a4
Sonu Kushwaha <kushwahasonu1319@gmail.com>
2025-03-25 23:10:00 +0530

    LinkedListBasedSimpleQueue
    LinkedListBasedSimpleQueue

---
+++ DS04_Queue/src/com/app/LinkedListBasedSimpleQueue.java
@@ -0,0 +1,82 @@
+package com.app;
+
+public class LinkedListBasedSimpleQueue<T> {
+
+	Node<T> front, rear;
+
+	public LinkedListBasedSimpleQueue() {
+		this.front = null;
+		this.rear = null;
+	}
+
+	public boolean isEmpty() {
+
+		if (front == null || rear == null) {
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+
+	public void enQueue(T data) {
+		Node<T> newNode = new Node<T>(data);
+
+		if (isEmpty()) {
+			rear = front = newNode;
+		} else {
+			rear.next = newNode;
+			rear = newNode;
+		}
+	}
+
+	public T deQueue() {
+
+		if (isEmpty()) {
+			System.out.println("Queue is empty,Can't de queue element");
+			return null;
+		}
+
+		else {
+
+			T data = front.data;
+			front = front.next;
+			if (front == null)
+				rear = null;
+			return data;
+		}
+	}
+
+	public void display() {
+
+		if (isEmpty()) {
+			System.out.println("Queue is empty, no data to display");
+		} else {
+			Node<T> curr_node = front;
+			while (curr_node != null) {
+				System.out.println(curr_node.data + " ");
+				curr_node = curr_node.next;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		LinkedListBasedSimpleQueue<Integer> queue = new LinkedListBasedSimpleQueue<>();
+
+		queue.display();
+		queue.enQueue(12);
+		queue.enQueue(13);
+		queue.enQueue(14);
+		queue.enQueue(15);
+		queue.display();
+		System.out.println("De queue element");
+		queue.deQueue();
+		queue.deQueue();
+		queue.deQueue();
+		queue.deQueue();
+		queue.deQueue();
+		queue.display();
+	}
+}
---
+++ DS04_Queue/src/com/app/Node.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Node<T> {
+	T data;
+	Node<T> next;
+	
+	public Node(T data) {
+		super();
+		this.data = data;
+		this.next = null;
+	}
+	
+	
+
+}
