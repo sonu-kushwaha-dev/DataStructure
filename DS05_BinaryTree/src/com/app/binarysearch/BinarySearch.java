a253d142ccd83569c5345a3a0b20ba70a539cbc3
Sonu Kushwaha <kushwahasonu1319@gmail.com>
2025-04-13 21:21:03 +0530

    Created BinarySearch.java
    BinarySearch

---
+++ DS05_BinaryTree/src/com/app/binarysearch/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.app.binarysearch;
+
+public class BinarySearch {
+
+	public int search(int[] nums, int target) {
+
+		int low = 0, high = nums.length - 1,mid=0;
+
+		while (low <= high) {
+			
+			mid = low + (high - low) / 2;
+			
+			if (nums[mid] == target) {
+				return mid;
+			} else if (target < nums[mid]) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		BinarySearch bs = new BinarySearch();
+		int[] nums = { 1, 3, 7, 9, 13, 15, 17 };
+
+		System.out.println(bs.search(nums, 17));
+
+	}
+
+}
